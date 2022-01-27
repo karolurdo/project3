2ca7c941b099cd830289c618108edcce8580c173
Urdo <karol.ugarenko@gmail.com>
2022-01-27 20:07:25 +0100

    add coments

--- project3.js
+++ project3.js
@@ -1,14 +1,13 @@
 
 
-let size = 10; 
-const btn = document.querySelector("button"); 
-const items = document.querySelectorAll("li");
+let size = 10; // font size 10px
+const btn = document.querySelector("button"); // querySelector button 
+const items = document.querySelectorAll("li"); // querySelectro all "li"
 
-
-
-btn.addEventListener('click', function() {
+btn.addEventListener('click', function() { // Listener on click
         size++; 
-    for (let i = 0; i < items.length; i++){
-        items[i].style.display = "block"; 
-        items[i].style.fontSize = size +"px"; }})
+    for (let i = 0; i < items.length; i++) { // forEach
+        items[i].style.display = "block"; // change style
+        items[i].style.fontSize = size +"px";  // change style
+    }})
     
