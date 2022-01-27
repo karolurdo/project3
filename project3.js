d394275a98153607d8dfe02e928ce18b15856ced
Urdo <karol.ugarenko@gmail.com>
2022-01-27 20:05:44 +0100

    fix code

--- project3.js
+++ project3.js
@@ -4,15 +4,11 @@ let size = 10;
 const btn = document.querySelector("button"); 
 const items = document.querySelectorAll("li");
 
-const showLi = function() {
-console.log("work");
-size++; 
-for (let i = 0; i < items.length; i++) { // foreach
-    items[i].style.display = "block"; 
-    items[i].style.fontSize = size +"px";
-}
 
 
-};
-
-btn.addEventListener('click', showLi); // Listerner
+btn.addEventListener('click', function() {
+        size++; 
+    for (let i = 0; i < items.length; i++){
+        items[i].style.display = "block"; 
+        items[i].style.fontSize = size +"px"; }})
+    
